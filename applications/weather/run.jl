057dcf980d34370324f24762a2b619ef90c020be
hannahilea <hannahilea@users.noreply.github.com>
2022-02-21 17:19:18 -0500

    add logging statements

--- applications/weather/run.jl
+++ applications/weather/run.jl
@@ -1,8 +1,10 @@
 # Display weather on the flipdots board!
+@info "Setting up environment..."
 using Pkg
 Pkg.resolve()
 Pkg.instantiate()
 
+@info "Loading dependencies..."
 using FlipBoard
 using HTTP
 using JSON
@@ -12,6 +14,7 @@ using JSON
 # Hacky check to see if we're on the pi, to prevent trying to connect to port that
 # isn't set up from laptop. For other systems, update `shared_srl` to point to
 # the relevant serial port!
+@info "Setting up serial port..."
 shared_srl = Sys.islinux() ? open_srl(; portname="/dev/ttyS0", baudrate=57600) : IOBuffer()
 
 dots_sink = AlphaZetaSrl(; address=0x00, srl=shared_srl)
@@ -31,9 +34,10 @@ const CLOUD = map(FlipBoard.seg_to_bits,
                   [[5, 6], [3, 4, 6], [2, 6], [3, 6], [4, 6], [4, 6], [5, 6]])
 
 # Set up weather; default loc_str is Somerville, MA
-function get_weather(; location="42.3876,-71.0995", verbose=false)
+function get_weather(; location="42.3876,-71.0995")
     try
-        verbose && (@info "https://api.weather.gov/points/$location")
+        @debug "Getting weather..." "https://api.weather.gov/points/$location"
+        @info "Fetching forecast links..."
         data = JSON.parse(String(HTTP.request("GET",
                                               "https://api.weather.gov/points/$location").body))
 
@@ -41,8 +45,9 @@ function get_weather(; location="42.3876,-71.0995", verbose=false)
         # all of those dependencies. So! Doing it the stupid brittle way here :)
         forecast_url = data["properties"]["forecast"]
         hourly_url = data["properties"]["forecastHourly"]
-        verbose && (@info "Location urls: " forecast_url hourly_url)
+        @debug "Location urls: " forecast_url hourly_url
 
+        @info "Fetching forecasts..."
         forecast_result = JSON.parse(String(HTTP.request("GET", forecast_url).body))
         hourly_result = JSON.parse(String(HTTP.request("GET", hourly_url).body))
         return forecast_result, hourly_result
@@ -55,6 +60,7 @@ end
 function _get_weather_icon_from_hourly(current_hourly)
     # Check for rest of day through midnight
     # (_after_ midnight, is for next day)
+    @info "Determining weather icon..."
     i = findfirst(c -> contains(c["startTime"], "T00"), current_hourly)
     weather_str = lowercase(join(map(c -> c["shortForecast"], current_hourly[1:i]), " "))
 
@@ -64,7 +70,8 @@ function _get_weather_icon_from_hourly(current_hourly)
 
     # If not, what is it like right now?
     if first(current_hourly)["isDaytime"]
-        return contains(lowercase(first(current_hourly)["shortForecast"]), "sun") ? SUN : CLOUD
+        return contains(lowercase(first(current_hourly)["shortForecast"]), "sun") ? SUN :
+               CLOUD
     end
     return NIGHT
 end
@@ -73,12 +80,13 @@ end
 format_weather(::Any) = ("N/A", "Unable to fetch weather")
 
 function format_weather(forecast, hourly_forecast)
+    @info "Formatting output..."
     current = forecast["properties"]["periods"]
     current_hourly = hourly_forecast["properties"]["periods"]
 
     weather_icon = _get_weather_icon_from_hourly(current_hourly)
-    short_str = vcat(text_to_dots_bytes(string("   ", first(current_hourly)["temperature"], "°")),
-                                         weather_icon)
+    short_str = vcat(text_to_dots_bytes(string("   ", first(current_hourly)["temperature"],
+                                               "°")), weather_icon)
     long_str = string("Now: ", first(current_hourly)["temperature"], "° ",
                       first(current)["shortForecast"], "! ", current[2]["name"], ": ",
                       current[2]["shortForecast"], "!")
@@ -90,11 +98,11 @@ function update_with_current_weather(; scroll_long_msg=true)
     bytes_static, bytes_scroll = format_weather(get_weather()...)
 
     # Display it!
+    @info "Desplaying output..."
     scroll_long_msg && scroll_bytes(dots_sink, bytes_scroll; loopcount=1)
     display_bytes(dots_sink, bytes_static)
     return nothing
 end
-
 
 if isinteractive()
     update_with_current_weather()
