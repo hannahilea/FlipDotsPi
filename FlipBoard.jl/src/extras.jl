9f4cd257583238589f9e202eddab616b987a664d
hannahilea <hannahilea@users.noreply.github.com>
2022-02-21 13:33:34 -0500

    wip merge in main

--- FlipBoard.jl/src/extras.jl
+++ FlipBoard.jl/src/extras.jl
@@ -32,7 +32,7 @@ const rhythm2 = ["1 1 1 1 1 1 1 1 1 1 1 1 1 1",
                  ""]
 
 # TODO helper for no srl
-function drumbeat_snippet2(srl, board, phrases=rhythm1; pause=.1)
+function drumbeat_snippet2(srl, board, phrases=rhythm1; pause=0.1)
     for i in 1:16
         for p in phrases
             send_transmission(srl, text_to_bytes(board, p))
@@ -40,3 +40,9 @@ function drumbeat_snippet2(srl, board, phrases=rhythm1; pause=.1)
         end
     end
 end
+
+#####
+##### Matrix plotting functionalities
+#####
+
+#TODO!
